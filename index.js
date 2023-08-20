2906d8a60cb069b8ca11caf7b8a7f71328251cd8
kumavis <aaron@kumavis.me>
2023-08-19 20:49:32 -1000

    refactor

--- index.js
+++ index.js
@@ -1,26 +1,59 @@
-const Gpio = require('onoff').Gpio;
-const out1 = new Gpio(18, 'out');
-const out2 = new Gpio(23, 'out');
-const out3 = new Gpio(24, 'out');
-const out4 = new Gpio(25, 'out');
-const in1 = new Gpio(20, 'in', 'both');
-const in2 = new Gpio(21, 'in', 'both');
+const Gpio = require('onoff').Gpio
 
-const allPins = [
-  out1, out2, in1
-]
+const gadget = makeGadget(10, 10)
+gadget.addOutput(18, '18 blue')
+gadget.addOutput(23, '23 white')
+gadget.addOutput(24, '24 yellow')
+gadget.addOutput(25, '25 red')
+gadget.addInput(20, '20')
+gadget.addInput(21, '21')
 
-const clocks = [
-  makeClock(out1, 10, '18 blue'),
-  makeClock(out2, 20, '23 white'),
-  makeClock(out3, 30, '24 yellow'),
-  makeClock(out4, 40, '25 red'),
-]
 
-makeReceiver(in1, '20')
-makeReceiver(in2, '21')
+function makeGadget (intervalStart, intervalStep) {
+  const allPins = []
+  const clocks = []
+  const receivers = []
 
-function makeReceiver (gpioPin, label) {
+  const gadget = {
+    addOutput (gpioPinNumber, label) {
+      const gpioPin = new Gpio(gpioPinNumber, 'out');
+      allPins.push(gpioPin)
+      const interval = intervalStart + (clocks.length * intervalStep)
+      const clock = makeClockEmitter(gpioPin, interval, label)
+      clocks.push(clock)
+      allPins.push(gpioPin)
+      return clock
+    },
+    addInput (gpioPinNumber, label) {
+      const gpioPin = new Gpio(gpioPinNumber, 'in', 'both');
+      allPins.push(gpioPin)
+      const receiver = makeReceiver(gpioPin, label, intervalStart, intervalStep, clocks)
+      receivers.push(receiver)
+      return receiver
+    },
+    exit () {
+      // stop all clocks
+      clocks.forEach(clock => clock.stop())
+      // unexport all pins
+      allPins.forEach(pin => {
+        try {
+          pin.unexport()
+        } catch (_err) {
+          // ignore error
+        }
+      })
+    }
+  }
+
+  // exit on Ctrl-C
+  process.on('SIGINT', _ => {
+    gadget.exit()
+  });
+
+  return gadget
+}
+
+function makeReceiver (gpioPin, label, intervalStart, intervalStep, clocks) {
   let lastTimestamp = undefined
   let lastClock = undefined
   let timeoutRef = undefined
@@ -31,9 +64,11 @@ function makeReceiver (gpioPin, label) {
     const curr = Date.now()
     if (lastTimestamp !== undefined) {
       const delta = curr - lastTimestamp
-      const rounded = Math.round(delta / 10) * 10;
+      const rounded = intervalStart + Math.round((delta - intervalStart) / intervalStep) * intervalStep;
       const sourceClock = clocks.find(clock => clock.interval === rounded)
+      // found a clock
       if (sourceClock) {
+        // update connection
         if (lastClock !== sourceClock) {
           console.log(`${label}: connected ${sourceClock.label} (${delta})`)
           lastClock = sourceClock
@@ -45,7 +80,9 @@ function makeReceiver (gpioPin, label) {
           lastClock = undefined
         }, sourceClock.interval * 1.5)
       } else {
+        // no clock found
         if (lastClock !== undefined) {
+          // set as disconnected
           console.log(`${label}: disconnected (bad clock)`)
           lastClock = undefined
         }
@@ -55,28 +92,16 @@ function makeReceiver (gpioPin, label) {
   });
 }
 
-
-function makeClock (gpioPin, interval, label) {
+function makeClockEmitter (gpioPin, interval, label) {
   let value = 0
-  setInterval(() => {
+  const intervalRef = setInterval(() => {
     value = invert(value)
     gpioPin.writeSync(value)
   }, interval)
-  return { interval, label }
+  const stop = () => clearInterval(intervalRef)
+  return { interval, label, stop }
 }
 
 function invert (value) {
   if (value) { return 0 } else { return 1 }
-}
-
-process.on('SIGINT', _ => {
-  exit()
-});
-
-// exit()
-
-function exit () {
-  allPins.forEach(pin => {
-    pin.unexport()
-  })
 }
